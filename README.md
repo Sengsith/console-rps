b663653e2a76b9368ecbe21968c5ac27713a12db
Angelo Rosario <angelorosario10@gmail.com>
2024-03-21 14:08:14 -0700

    update script.js README.md
    implemented 5 rounds RPS, post reflections in README

--- README.md
+++ README.md
@@ -3,7 +3,14 @@
 Table of Contents
 
 - [Before attempt](#before-attempt)
+- [After attempt](#after-attempt)
 
 ## Before attempt
 
 I have some javascript experience already so I'm hoping that I get a decent refresher on it and can complete this in a relatively fast time.
+
+## After attempt
+
+Overall a very fast and simple challenge, had to refresh on how to create and reference Enums and how to randomize numbers.
+
+[BACK TO TOP](#console-rps)
--- script.js
+++ script.js
@@ -15,35 +15,35 @@ function playRPS(playerSelection, computerSelection) {
   switch (playerSelection) {
     case CHOICES.ROCK:
       if (computerSelection === CHOICES.PAPER) {
-        console.log("Rock loses to paper, computer wins!");
+        return "Rock loses to paper, computer wins!";
       } else if (computerSelection === CHOICES.SCISSORS) {
-        console.log("Rock beats scissors, player wins!");
+        return "Rock beats scissors, player wins!";
       } else {
-        console.log("Rock clashes with rock, tie!");
+        return "Rock clashes with rock, tie!";
       }
-      break;
     case CHOICES.PAPER:
       if (computerSelection === CHOICES.ROCK) {
-        console.log("Paper beats rock, player wins!");
+        return "Paper beats rock, player wins!";
       } else if (computerSelection === CHOICES.SCISSORS) {
-        console.log("Paper loses to scissors, computer wins!");
+        return "Paper loses to scissors, computer wins!";
       } else {
-        console.log("Paper clashes with paper, tie!");
+        return "Paper clashes with paper, tie!";
       }
-      break;
     case CHOICES.SCISSORS:
       if (computerSelection === CHOICES.ROCK) {
-        console.log("Scissors loses to rock, computer wins!");
+        return "Scissors loses to rock, computer wins!";
       } else if (computerSelection === CHOICES.PAPER) {
-        console.log("Scissors beats paper, player wins!");
+        return "Scissors beats paper, player wins!";
       } else {
-        console.log("Scissors clashes with scissors, tie!");
+        return "Scissors clashes with scissors, tie!";
       }
-      break;
   }
 }
 
-let playerChoice = prompt("Rock, paper, or scissors?").toLowerCase();
-let computerChoice = getComputerChoice();
-
-playRPS(playerChoice, computerChoice);
+// Run 5 rounds of rps
+const ROUNDS = 5;
+for (let i = 0; i < ROUNDS; i++) {
+  let playerChoice = prompt("Rock, paper, or scissors?").toLowerCase();
+  let computerChoice = getComputerChoice();
+  console.log(playRPS(playerChoice, computerChoice));
+}
