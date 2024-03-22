9321b5596b06bd71f8026e655327d72b88954733
Angelo Rosario <angelorosario10@gmail.com>
2024-03-22 16:47:43 -0700

    update index.html script.js
    simple UI added, no styling

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const CHOICES = {
   SCISSORS: "scissors",
 };
 
+const GAME_STATE = {
+  playerScore: 0,
+  computerScore: 0,
+  gamesPlayed: 0,
+};
+
 function getComputerChoice() {
   // Randomize choice
   let rand = Math.floor(Math.random() * 3);
@@ -11,28 +17,53 @@ function getComputerChoice() {
   return CHOICES[Object.keys(CHOICES)[rand]];
 }
 
+function playerWins() {
+  GAME_STATE.playerScore++;
+  updateScoreBoard(PLAYER_SCORE, GAME_STATE.playerScore);
+}
+
+function computerWins() {
+  GAME_STATE.computerScore++;
+  updateScoreBoard(COMPUTER_SCORE, GAME_STATE.computerScore);
+}
+
+function incrementGamesPlayed() {
+  GAME_STATE.gamesPlayed++;
+  updateScoreBoard(GAMES_PLAYED, GAME_STATE.gamesPlayed);
+}
+
+function updateScoreBoard(element, value) {
+  element.textContent = value;
+}
+
 function playRPS(playerSelection, computerSelection) {
   switch (playerSelection) {
     case CHOICES.ROCK:
       if (computerSelection === CHOICES.PAPER) {
+        computerWins();
         return "Rock loses to paper, computer wins!";
       } else if (computerSelection === CHOICES.SCISSORS) {
+        playerWins();
         return "Rock beats scissors, player wins!";
       } else {
         return "Rock clashes with rock, tie!";
       }
     case CHOICES.PAPER:
       if (computerSelection === CHOICES.ROCK) {
+        playerWins();
         return "Paper beats rock, player wins!";
       } else if (computerSelection === CHOICES.SCISSORS) {
+        computerWins();
         return "Paper loses to scissors, computer wins!";
       } else {
         return "Paper clashes with paper, tie!";
       }
     case CHOICES.SCISSORS:
       if (computerSelection === CHOICES.ROCK) {
+        computerWins();
         return "Scissors loses to rock, computer wins!";
       } else if (computerSelection === CHOICES.PAPER) {
+        playerWins();
         return "Scissors beats paper, player wins!";
       } else {
         return "Scissors clashes with scissors, tie!";
@@ -42,8 +73,12 @@ function playRPS(playerSelection, computerSelection) {
 
 const BTNS_CONTAINER = document.querySelector(".btns-container");
 const RESULTS = document.querySelector(".results");
-let playerChoice,
-  computerChoice = "";
+const PLAYER_SCORE = document.querySelector(".player-score");
+const COMPUTER_SCORE = document.querySelector(".computer-score");
+const GAMES_PLAYED = document.querySelector(".games-played");
+
+let playerChoice = "";
+let computerChoice = "";
 
 BTNS_CONTAINER.addEventListener("click", (e) => {
   // Don't want to do anything if clicking within container, while missing a button
@@ -52,4 +87,5 @@ BTNS_CONTAINER.addEventListener("click", (e) => {
   playerChoice = CHOICES[e.target.className.toUpperCase()];
   computerChoice = getComputerChoice();
   RESULTS.textContent = playRPS(playerChoice, computerChoice);
+  incrementGamesPlayed();
 });
